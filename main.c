b0d71db1f241e685d279ae8a1e02556d7cdb1cba
Yuva MEHDI <ymehdi@e3r12p23.42.fr>
2019-04-05 19:27:53 +0200

    adding a makefile

---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymehdi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/05 15:23:54 by ymehdi            #+#    #+#             */
+/*   Updated: 2019/04/05 17:06:15 by ymehdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	argc = 0;
+	//printf("le vrai strlen :%lu\nle miens :%lu", strlen(argv[1]), ft_strlen(NULL));
+	//ft_putstr(argv[1]);
+	ft_putendl(argv[1]);
+	return 0;
+}
